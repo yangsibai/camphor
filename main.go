af282779dce898aa9918c3e851ec86f18a302c18
Yangsibai <yangsibai@gmail.com>
2016-02-13 09:36:38 +0800

    add routes

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"github.com/julienschmidt/httprouter"
 	"github.com/unrolled/render"
-	"gopkg.in/mgo.v2/bson"
 	"log"
 	"net/http"
 	"os"
-	"time"
 )
 
 var config Configuration
@@ -21,43 +18,9 @@ func main() {
 	router.GET("/posts", AllPosts)
 	router.GET("/post", AddPostPage)
 	router.POST("/post", AddPost)
+	router.POST("/posts", AllPosts)
+	log.Println("camphor run at ", config.Addr)
 	log.Fatal(http.ListenAndServe(config.Addr, router))
-}
-
-func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	ren.HTML(w, http.StatusOK, "index", "Camphor")
-}
-
-func AllPosts(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	posts, err := getAllPosts()
-	if err != nil {
-		fmt.Fprint(w, err.Error())
-		return
-	}
-	if posts == nil || len(posts) == 0 {
-		fmt.Fprint(w, "no posts")
-		return
-	}
-
-	ren.HTML(w, http.StatusOK, "posts", posts)
-}
-
-func AddPostPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	ren.HTML(w, http.StatusOK, "add_post", nil)
-}
-
-func AddPost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	post := Post{
-		ID:        bson.NewObjectId(),
-		Body:      r.FormValue("body"),
-		CreatedAt: time.Now(),
-	}
-	err := storePost(&post)
-	if err != nil {
-		fmt.Fprint(w, err.Error())
-		return
-	}
-	http.Redirect(w, r, "/posts", http.StatusTemporaryRedirect)
 }
 
 func readConfig() (error, Configuration) {
---
+++ routes.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"github.com/julienschmidt/httprouter"
+	"gopkg.in/mgo.v2/bson"
+	"net/http"
+	"time"
+)
+
+func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	ren.HTML(w, http.StatusOK, "index", "Camphor")
+}
+
+func AllPosts(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	posts, err := getAllPosts()
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+		return
+	}
+	if posts == nil || len(posts) == 0 {
+		fmt.Fprint(w, "no posts")
+		return
+	}
+
+	ren.HTML(w, http.StatusOK, "posts", posts)
+}
+
+func AddPostPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	ren.HTML(w, http.StatusOK, "add_post", nil)
+}
+
+func AddPost(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	post := Post{
+		ID:        bson.NewObjectId(),
+		Body:      r.FormValue("body"),
+		CreatedAt: time.Now(),
+	}
+	err := storePost(&post)
+	if err != nil {
+		fmt.Fprint(w, err.Error())
+		return
+	}
+	http.Redirect(w, r, "/posts", http.StatusTemporaryRedirect)
+}
