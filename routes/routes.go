b056fa25d19b6f7645c98287f4fe479ce20db342
Yangsibai <yangsibai@gmail.com>
2016-03-08 23:17:45 +0800

    update css

--- routes/routes.go
+++ routes/routes.go
@@ -27,20 +27,6 @@ func Index(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	ren.HTML(w, http.StatusOK, "index", posts)
 }
 
-//func AllPosts(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-//posts, err := getAllPosts()
-//if err != nil {
-//fmt.Fprint(w, err.Error())
-//return
-//}
-//if posts == nil || len(posts) == 0 {
-//fmt.Fprint(w, "no posts")
-//return
-//}
-
-//ren.HTML(w, http.StatusOK, "posts", posts)
-//}
-
 func AddPostPage(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 	ren.HTML(w, http.StatusOK, "add_post", nil)
 }
