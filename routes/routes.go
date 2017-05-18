dff902291210c9969fb4a932a799be4f8362d684
Yangsibai <yangsibai@gmail.com>
2017-05-18 11:59:44 +0800

    remove image from homepage and add index of post

--- routes/routes.go
+++ routes/routes.go
@@ -10,6 +10,8 @@ import (
 	"gopkg.in/mgo.v2/bson"
 	"html/template"
 	"net/http"
+	"sort"
+	"strconv"
 	"strings"
 	"time"
 )
@@ -20,7 +22,59 @@ func init() {
 	ren = render.New(render.Options{
 		Directory: "tmpls",
 		Layout:    "layout",
+		Funcs: []template.FuncMap{template.FuncMap{
+			"minus": func(a, b int) int {
+				return a - b
+			},
+			"roman": func(num int) string {
+				return getRoman(num)
+			},
+		}},
 	})
+}
+
+func getRoman(number int) string {
+	// create a denary_number:roman_symbol map
+	romanMap := map[int]string{
+		1: "I", 4: "IV", 5: "V", 9: "IX", 10: "X", 40: "XL", 50: "L",
+		90: "XC", 100: "C", 400: "CD", 500: "D", 900: "CM", 1000: "M",
+	}
+	// create a slice of slices
+	rows := len(romanMap)
+	matrix := make([][]string, rows)
+	// create a slice of the map keys
+	var key_slice []int
+	// range of a map returns key, value pair
+	// value is not needed so use blank identifier _
+	for k, _ := range romanMap {
+		key_slice = append(key_slice, k)
+	}
+	// sort the slice in place, highest number first (decending)
+	sort.Sort(sort.Reverse(sort.IntSlice(key_slice)))
+	// create a slice of romanMap content slices
+	row := 0
+	// range of a slice returns index, value pair
+	// index not needed so use blank identifier _
+	for _, key := range key_slice {
+		// convert int key to string key
+		skey := strconv.Itoa(key)
+		matrix[row] = []string{skey, romanMap[key]}
+		row++
+	}
+	result := ""
+	for _, item := range matrix {
+		// convert string to int
+		den, err := strconv.Atoi(item[0])
+		if err != nil {
+			panic(err)
+		}
+		sym := item[1]
+		for number >= den {
+			result += sym
+			number -= den
+		}
+	}
+	return result
 }
 
 func responseError(w http.ResponseWriter, err error) {
